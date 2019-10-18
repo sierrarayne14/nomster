dec8452b403f2dca69497c6f7df3bfff82a0f4d7
Sierra Ponce <sierrarayne0220@gmail.com>
2019-10-18 17:22:00 +0000

    make rating dropdown

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
 belongs_to :user
 belongs_to :place
+
+RATING = {
+    'one star': '1_star',
+    'two stars': '2_stars',
+    'three stars': '3_stars',
+    'four stars': '4_stars',
+    'five stars': '5_stars'
+  }
 end
