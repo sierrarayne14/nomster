8f736d82950faa204a5ce4de0f3fc6bfa4d252ac
Sierra Ponce <sierrarayne0220@gmail.com>
2019-10-25 17:03:55 +0000

    Add a photo

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,10 +2,13 @@ class Place < ApplicationRecord
   belongs_to :user 
   has_many :comments
 
-  
+
   geocoded_by :address
   after_validation :geocode 
 
   
-  validates :name, presence: true
+  validates :name, presence: true, length: {minimum: 3}
+  validates :description, presence: true
+  validates :address, presence: true
 end
+
--- db/migrate/20191019172708_create_photos.rb
+++ db/migrate/20191019172708_create_photos.rb
@@ -1,8 +1,12 @@
 class CreatePhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :photos do |t|
-
+      t.integer :place_id 
+      t.integer :user_id
+      t.text :caption
       t.timestamps
     end
+    add_index :photos, [user_id, place_id]
+    add_index :photos, place_id
   end
 end
