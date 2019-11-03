f7806ad93f4ad9f896c7b4f6cf4074436e9c4ff6
Sierra Ponce <sierrarayne0220@gmail.com>
2019-11-03 21:45:06 +0000

    Add a photo

--- app/controllers/photos_contoller.rb
+++ app/controllers/photos_contoller.rb
@@ -1,5 +1,11 @@
 class PhotosController < ApplicationController 
+  def create
+    @place = Place.find(params[:place_id])
+    @place.photo.create(photo_params.merge(user: current_user))
+    redirect_to place_path(@place)
+  end
+
   def photo_params 
-    params.require(:picture).permit(:caption)
+    params.require(:photo).permit(:picture, :caption)
   end 
 end 
